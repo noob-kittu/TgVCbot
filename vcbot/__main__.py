291a226f6a535c3ebefa797251cdcb0034838aef
Kittu <70826024+noob-kittu@users.noreply.github.com>
2021-10-26 02:47:03 +0530

    var one

--- vcbot/__main__.py
+++ vcbot/__main__.py
@@ -1,4 +1,10 @@
 from vcbot import app
+from importlib import import_module
+from vcbot.plugins import ALL_MODULES
+
+
+for module_name in ALL_MODULES:
+    imported_module = import_module("vcbot.plugins." + module_name)
 
 app.start()
 
